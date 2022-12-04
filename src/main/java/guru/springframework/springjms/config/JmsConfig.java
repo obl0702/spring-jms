54d96eced3dcfb42d3db0037f8ffa7fe7cb37642
boonloong <obl0702@gmail.com>
2022-12-04 22:07:58 +0800

    Send and Receive JMS Message. Closed #7

--- src/main/java/guru/springframework/springjms/config/JmsConfig.java
+++ src/main/java/guru/springframework/springjms/config/JmsConfig.java
@@ -10,6 +10,7 @@ import org.springframework.jms.support.converter.MessageType;
 public class JmsConfig {
 
     public static final String MY_QUEUE = "my-ongbl";
+    public static final String MY_SEND_RCV_QUEUE = "replybacktome";
 
     @Bean
     public MessageConverter messageConverter(){
