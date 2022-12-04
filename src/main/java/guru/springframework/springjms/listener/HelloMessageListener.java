817e9a40c6aeb269102caea2bb01a18250f899ba
boonloong <obl0702@gmail.com>
2022-12-04 18:05:28 +0800

    Receive JMS Message. Closed #6

---
+++ src/main/java/guru/springframework/springjms/listener/HelloMessageListener.java
@@ -0,0 +1,23 @@
+package guru.springframework.springjms.listener;
+
+import guru.springframework.springjms.config.JmsConfig;
+import guru.springframework.springjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import javax.jms.Message;
+
+@Component
+public class HelloMessageListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage,
+                       @Headers MessageHeaders headers,
+                       Message message){
+
+        System.out.println("New Message Received");
+        System.out.println(helloWorldMessage);
+    }
+}
